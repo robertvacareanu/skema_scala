3f33e92d41d39ae61356f91d00027dc92667a524
Robert Vacareanu <robert.vacareanu@gmail.com>
2023-12-19 21:37:43 -0700

    Neural Grounder implementation

--- src/main/scala/org/clulab/scala_grounders/grounding/NeuralGrounder.scala
+++ src/main/scala/org/clulab/scala_grounders/grounding/NeuralGrounder.scala
@@ -3,29 +3,63 @@ package org.clulab.scala_grounders.grounding
 import org.clulab.scala_grounders.model.{DKG, GroundingResultDKG, GroundingDetails}
 
 import ai.onnxruntime.{OnnxTensor, OrtEnvironment, OrtSession}
-import org.clulab.scala_transformers.encoder.Encoder
+import org.clulab.scala_transformers.tokenizer.Tokenizer
+import org.clulab.scala_transformers.tokenizer.jni.ScalaJniTokenizer
+import java.util.{HashMap => JHashMap}
 
 class NeuralGrounder(modelPath: String, threshold: Double) extends Grounder {
+
+  private val nn = new UnderlyingNeuralNetworkImplementation(modelPath)
 
   override def getName: String = "Neural Grounder"
 
   def ground(text: String, groundingTargets: Seq[DKG], k: Int): Stream[GroundingResultDKG] = {
     // groundingTargets.toStream.take(k).map { it => GroundingResultDKG(0, it, GroundingDetails(getName)) }
-    ???
+    groundingTargets.map { dkg =>
+      val score = nn.forwardSingle(text, dkg.name)
+      GroundingResultDKG(
+        score=score,
+        dkg=dkg,
+        groundingDetails = GroundingDetails(getName, Some("name"), None)
+      )
+    }.sortBy(-_.score).toStream
   }
-  
+
+  private class UnderlyingNeuralNetworkImplementation(onnxModelPath: String) {
+    private val env       = OrtEnvironment.getEnvironment()
+    private val session   = env.createSession(onnxModelPath, new OrtSession.SessionOptions())
+    private val tokenizer = ScalaJniTokenizer("microsoft/deberta-v3-base", addPrefixSpace=false)
+
+    def forwardSingle(text1: String, text2: String): Float = {
+      val inputIds1 = tokenizer.tokenize(text1.split(" ")).tokenIds
+      // FIXME maybe not all models add [CLS] at the beginning
+      val inputIds2 = tokenizer.tokenize(text2.split(" ")).tokenIds.tail // `.tail` ensures that we do not capture the `[CLS]` token that is being added to all sequences
+      val inputIds  = Array.concat(inputIds1, inputIds2)
+      val attentionMask = Array.fill(inputIds1.length + inputIds2.length)(1l)
+      val tokenTypeIds  = Array.concat(Array.fill(inputIds1.length)(0l), Array.fill(inputIds2.length)(1l))
+      val inputs = new JHashMap[String, OnnxTensor]()
+      inputs.put("input_ids",      OnnxTensor.createTensor(env, Array(inputIds.map(_.toLong))))
+      inputs.put("attention_mask", OnnxTensor.createTensor(env, Array(attentionMask.map(_.toLong))))
+      // inputs.put("token_type_ids", OnnxTensor.createTensor(env, Array(tokenTypeIds))) // For DeBERTa it looks like `token_type_ids` are not used; Same result given when all `0`, when `correct` and when 1 item is 4 (when one item is 4 we should have seen an error)
+      // val encoderOutput = encoderSession.run(inputs).get(0).getValue.asInstanceOf[Array[Array[Array[Float]]]]
+      val result = session.run(inputs).get(0).getValue().asInstanceOf[Array[Array[Float]]].head
+      val lastSoftmaxed = Math.exp(result.last) / (Math.exp(result.head) + Math.exp(result.last))
+      lastSoftmaxed.toFloat
+    }
+  }
+
 }
+
 
 object NGrounder extends App {
-    val env = OrtEnvironment.getEnvironment()
-    // val session = env.createSession("/home/rvacareanu/projects_7_2309/skema_python/results/2312/model_onnx/model.onnx", new OrtSession.SessionOptions())
-    val encoder = Encoder.fromFile("/home/rvacareanu/projects_7_2309/skema_python/results/2312/model_onnx/model.onnx")
-
-    println("Hello, World!")
-    println(encoder.forward(Array(Array(1, 2, 3, 4, 5).map(_.longValue()), Array(1, 2, 3, 4, 5).map(_.longValue()))))
+    val grounder = new NeuralGrounder(modelPath = "/home/rvacareanu/projects_7_2309/skema_python/results/2312/onnx/model.onnx", 0.5)
+    val result = grounder.ground(
+      "exposed\nexposed (E)",
+      Seq(
+        DKG("id1", "exposed individuals", None, Seq.empty),
+        DKG("id2", "exposed contacts", None, Seq.empty),
+        DKG("id3", "number of cases exposed by local unknown exposure", None, Seq.empty),
+      )
+    )
+    result.foreach(println)
 }
-/**
- *  Integrate with
-  * https://github.com/ml4ai/skema/blob/main/skema/text_reading/scala/src/main/scala/org/ml4ai/skema/text_reading/grounding/Grounder.scala
-  * https://github.com/ml4ai/skema/blob/main/skema/text_reading/scala/src/main/scala/org/ml4ai/skema/text_reading/grounding/MiraEmbeddingsGrounder.scala#L167C7-L175
-  */
